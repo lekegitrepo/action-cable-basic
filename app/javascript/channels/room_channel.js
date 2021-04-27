7318c06ccf716332d69996addaeed0564a5565f6
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-04-27 08:56:19 +0100

    Update the channel name to be more flexible

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,17 +1,20 @@
 import consumer from "./consumer";
 
-consumer.subscriptions.create("RoomChannel", {
-  connected() {
-    console.log("Connected to room channel...");
-    // Called when the subscription is ready for use on the server
-  },
+consumer.subscriptions.create(
+  { channel: "RoomChannel", room_id: 1 },
+  {
+    connected() {
+      console.log("Connected to room channel...");
+      // Called when the subscription is ready for use on the server
+    },
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
 
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-    console.log("This is data from received function:", data);
-  },
-});
+    received(data) {
+      // Called when there's incoming data on the websocket for this channel
+      console.log("This is data from received function:", data);
+    },
+  }
+);
